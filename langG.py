53b28ab25ace13c7a2631dc2925cfedc42a84419
30209774 <saqib.mazhar@ucalgary.ca>
2025-08-01 01:22:50 -0600

    Simple AI wrapper

--- langG.py
+++ langG.py
@@ -1,56 +1,28 @@
 from dotenv import load_dotenv
 from typing import Annotated, Literal, List, Dict
 from langgraph.graph import StateGraph, START, END
-from langchain.chat_models import init_chat_model
-from pydantic import BaseModel, Field
 from typing_extensions import TypedDict
-import random
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 class AgentState(TypedDict):
-    name: str
-    number: List[int]
-    counter: int
+    messages: List[HumanMessage]
 
-def greet(state: AgentState)->AgentState:
-    '''Greeting node'''
+llm = ChatOpenAI(model="gpt-4o")
 
-    state['name'] = f"Hi there {state['name']} "
-    state['counter'] = 0
-
+def process(state:AgentState)-> AgentState:
+    response = llm.invoke(state['messages'])
+    print(f"\nAI: {response.content}")
     return state
-
-def randomnode(state:AgentState)->AgentState:
-    '''random node'''
-    state['number'].append(random.randint(0,10))
-    state['counter'] +=1
-
-    return state
-
-def scountinue(state:AgentState)->AgentState:
-    ''''''
-    if state["counter"] < 5:
-        print("Entering Loop", state["counter"])
-        return "loop"
-    else:
-        return "exit"
 
 graph = StateGraph(AgentState)
-graph.add_node("greeting", greet)
-graph.add_node("random", randomnode)
-graph.add_edge("greeting","random")
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+agent = graph.compile()
 
-graph.add_conditional_edges(
-    "random", # source  node
-    scountinue, # Action
-    {
-        "loop": "random", # self loop back to the same node
-        "exit": END # end the graph
-    }
-)
+user = input("Enter: ")
+agent.invoke({"messages": [HumanMessage(content=user)]})
 
-graph.set_entry_point("greeting")
-app = graph.compile()
-
-app.invoke({"name":"saqib", "number": [], "counter":-1})
